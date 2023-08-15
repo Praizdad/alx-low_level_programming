90fb2a3790329b9057e3168659d36bc18bc6136a
Praizdad <greatprincewill@yahoo.com>
2023-08-15 15:38:49 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times all alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
