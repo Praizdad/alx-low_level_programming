a9f75cb694dfb442eb641aaa7f9b122e28f073ee
Praizdad <greatprincewill@yahoo.com>
2023-08-15 18:16:08 -0700

    print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,6 +6,8 @@
  */
 int print_sign(int n)
 {
+	int negative = -1;
+	char ch = (char) negative;
 	if (n > 0)
 	{
 		_putchar('+');
@@ -19,6 +21,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (48);
+		return (ch);
 	}
 }
