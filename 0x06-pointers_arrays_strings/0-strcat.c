35f213174b0d69c3ea8bbcf68e02f8f3565d28f0
Praizdad <greatprincewill@yahoo.com>
2023-08-23 07:50:37 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: first input value
+ * @src: second input value
+ * Return: resulting string (dest)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int s1, s2, i;
+
+	for (s1 = 0; dest[s1] != '\0'; s1++)
+	{
+
+	}
+
+	for (s2 = 0; src[s2] != '\0'; s2++)
+	{
+
+	}
+	for (i = 0; i <= s2; i++)
+	{
+		dest[s1 + i] = src[i];
+	}
+	return (dest);
+}
