1c8e45216795edcfb726869ed4759cfc8300f8d4
Praizdad <greatprincewill@yahoo.com>
2023-09-11 07:48:27 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,17 +9,12 @@
  * Description: struct on a dog profile
  */
 
-/**
- * dog_t - new name for struct dog
- */
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
